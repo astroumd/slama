5903046b457870ebeaf1a93c28352e38bb927146
Peter Teuben <teuben@gmail.com>
2025-04-09 09:32:19 -0400

    removed, now obtained via their .in versions

--- valkey-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# valkey.conf cannot have a path in database file name, must be in $cwd
-REDIS_PORT=6380
-cp ${SLAMA}/data/sma-shared-variables.rdb .
-${SLAMA}/bin/valkey-server_sma ${SLAMA}/data/valkey_sma.conf --port ${REDIS_PORT} &
-sleep 3 # wait till server is loaded
-${SLAMA}/bin/smax-init.sh
