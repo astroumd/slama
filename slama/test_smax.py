293d2c345f447285005ba4888eadc8029563eb3d
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-04-29 13:29:50 -0700

    simple sanity test

---
+++ slama/test_smax.py
@@ -0,0 +1,12 @@
+# use this to test basic functioning of redis I/O
+import numpy as np
+from smax import SmaxRedisClient
+
+smax_client = SmaxRedisClient("localhost",redis_port=6380) # Replace localhost with redis hostname or IP.
+value = float(np.random.rand(1))
+table = "weather:forecast:gfs"
+key = "test_tau"
+smax_client.smax_share(table, key, value)
+result = smax_client.smax_pull(table, key)
+print(f"Input {table}:{key}={value}, fetched result {result.data} of type {result.type}")
+
