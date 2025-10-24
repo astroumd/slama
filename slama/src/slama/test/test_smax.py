3b81edfcaef716827a37a72d5236a5195233c5f6
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-10-24 14:21:30 -0700

    debug stuff

--- slama/src/slama/test/test_smax.py
+++ slama/src/slama/test/test_smax.py
@@ -11,24 +11,28 @@ table = "weather:forecast:gfs"
 key = "test_tau"
 smax_client.smax_share(table, key, value)
 result = smax_client.smax_pull(table, key)
-print(type(result))
+print(f"{type(result)=}")
+print(f"{type(result.data)=}")
 print(f"Input {table}:{key}={value}, fetched result {result.data} of type {result.type}")
+print(f"{result.smaxname=}")
 
 
+print("attributes= ",list(result.__dict__.keys()))
+if False:
 
-table = "weather:forecast:gfs"
-key = "test_array"
-smax_client.smax_subscribe(f"{table}:{key}")
+    table = "weather:forecast:gfs"
+    key = "test_array"
+    smax_client.smax_subscribe(f"{table}:{key}")
 
-# Share something, which send publish notifications automatically.
-value = [0.0, 1.12345, -1.54321, 100000.12345]
-smax_client.smax_share(table, key, value)
+    # Share something, which send publish notifications automatically.
+    value = [0.0, 1.12345, -1.54321, 100000.12345]
+    smax_client.smax_share(table, key, value)
 
-# Wait for publish notifications.
-result = smax_client.smax_wait_on_any_subscribed()
-print(result.data, result.type)
-smax_client.smax_subscribe(f"antenna:1*")
-result = smax_client.smax_wait_on_any_subscribed()
-print(result.data, result.type)
-print(result.__dict__)
+    # Wait for publish notifications.
+    result = smax_client.smax_wait_on_any_subscribed()
+    print(result.data, result.type)
+    smax_client.smax_subscribe(f"antenna:1*")
+    result = smax_client.smax_wait_on_any_subscribed()
+    print(result.data, result.type)
+    print(result.__dict__)
 
