3c2d5ef54c5d8a56c63a535e3cdfb70c5c2ee9d8
Marc Pound <22331890+mpound@users.noreply.github.com>
2026-03-25 08:14:53 -0700

    Add the CARMA system source catalog for use with fakeobs.py

--- slama/src/slama/fakeobs.py
+++ slama/src/slama/fakeobs.py
@@ -51,13 +51,21 @@ class FakeObs:
             for name, mp in self._mp_by_name.items()
         }
 
-        self._coord = SkyCoord(12.936435, -5.7893122222, unit=(u.hr, u.deg), frame="icrs")
-        self._source = "3c279"
         self._freq = 345.0
         self._vel = 0
-        self.setsource(self._source, self._coord, self._vel, self._freq)
+        self._catalog=catalog
+        self._cached_sources = {}
+        self._source_table = Table.read(self._catalog,comment="#",format='ascii.ipac',fast_reader=False,guess=False,data_start=3)
+        self._source_table.add_index("Source")
+        self._source = "3c279"
+        self._coord = self._get_source(self._source)
+        self.set_source(self._source, self._coord, self._vel, self._freq)
+        self.write("correlator:swarm:progress",0.0)
+        self.write("correlator:swarm:integration_time",0.0)
+        self.write("telescope:project:1:id","DDT 12345")
         self._lst = 10.33813
         self._ut = 15.77354516
+        self._tick_interval = 0.003*u.hr
         self.time()
         self.tsys(init=True)
         self._rh = 3.3428
@@ -67,10 +75,6 @@ class FakeObs:
         self._patm = 626.8255
         self.weather(init=True)
         self.slew(119.6513, 47.281)
-        self._catalog=catalog
-        self._source_table = Table.read(self._catalog)
-        self._source_table.add_index("Source")
-        self._cached_sources = {}
         
     def _get_source(self,name):
         """Get a SkyCoord of a source.  Will return cached SkyCoord if
@@ -79,12 +83,12 @@ class FakeObs:
         """
         NAME = name.upper()
         if NAME not in self._cached_sources:       
-            row = self._source_table.loc(NAME)
+            row = self._source_table.loc[NAME]
             s = SkyCoord(f'{row["RA"]} {row["DEC"]}',
                          frame='icrs',
                          unit=(u.hr,u.deg),
                          radial_velocity = row["Velocity"]*u.km/u.s)
-        self._cached_sourced[NAME] = s
+            self._cached_sources[NAME] = s
         return self._cached_sources[NAME]
 
     def write(self, canonical_name, value):
@@ -104,26 +108,27 @@ class FakeObs:
         except (TypeError, ValueError):
             return value
 
-    def observe(self, source):
+    def observe(self, source, obstime):
+        self.write("correlator:swarm:integration_time",obstime)
         self._source = source
         self._coord = self._get_source(source)
         self._vel = self._coord.radial_velocity.to(u.km/u.s).value
         self._freq = 230.538
 
-        self.setsource(self._source, self._coord, self._vel, self._freq)
+        self.set_source(self._source, self._coord, self._vel, self._freq)
         if source == "ORIMSR":
             az = 95
             el = 30
-        elif source == "3c279":
+        else:
             az = 170
             el = 45
 
         azincr = 0.004
         elincr = 0.003
-        timeincr = 0.003
+        timeincr = self.tick_interval.to("hr").value
         self.slew(az, el)
         i = 0
-        while i < 20:
+        while i < 20: #slewing
             i += 1
             if el > 89.0:
                 elincr = -0.016
@@ -142,15 +147,30 @@ class FakeObs:
             self.oops()
             self.time()
 
-    def setsource(self, source, coord, vel, freq):
+        # Integrating
+        print("Integrating...")
+        loopmax = obstime/10.0
+        i = 0
+        while i < loopmax:
+            print(f"progress {i*10}")
+            self.write("correlator:swarm:progress",i*10.0)
+            time.sleep(10.0)
+            i = i+1
+
+    def set_source(self, source, coord, vel, freq):
         self.write("RM:acc1:RM_SOURCE_C34", source)
         self.write("RM:acc1:RM_SVEL_KMPS_D", round(vel, 3))
         self.write("RM:acc1:RM_LOW_RX_LO_FREQUENCY_D", round(freq, 4))
         self.write("RM:acc1:RM_RA_CAT_HOURS_D", round(coord.ra.hour, 5))
         self.write("RM:acc1:RM_DEC_CAT_DEG_D", round(coord.dec.degree, 5))
 
+
+    @property
+    def tick_interval(self):
+        return self._tick_interval
+
     def tick(self):
-        timeincr = 0.003
+        timeincr = self.tick_interval.to("hr").value
         self._ut += timeincr
         self._lst += timeincr
 
@@ -278,12 +298,23 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog=progname)
     parser.add_argument("--loop", "-l", action="store", help="number of times to loop", default=4, type=int)
     parser.add_argument("--catalog", "-c", action="store", help="CARMA style catalog file to use for sources", default=None, type=str)
-    parser.add_argument("--fluxcal", "-f", action="store", help="flux calibrator", default=None, type=str)
-    parser.add_argument("--gaincal", "-g", action="store", help="gain calibrator", default=None, type=str)
-    parser.add_argument("--bandpass", "-f", action="store", help="bandpass calibrator", default=None, type=str)
-    parser.add_argument("--source", "-s", action="store", help="source aka science target", default=None, type=str)
+    parser.add_argument("--fluxcal", "-f", action="store", help="flux calibrator", default="3C273", type=str)
+    parser.add_argument("--bandpass", "-b", action="store", help="bandpass calibrator", default="3C279", type=str)
+    parser.add_argument("--gaincal", "-g", action="store", help="gain calibrator", default="0530+135", type=str)
+    parser.add_argument("--source", "-s", action="store", help="source aka science target", default="ORIMSR", type=str)
+    parser.add_argument("--time", "-t", action="store", help="how long to observe each target, in seconds", default=60, type=float)
     args = parser.parse_args()
     
     fo = FakeObs()
-    #fo.observe("3c279")
-    fo.mixerH()
+    print(f"Slewing to Flux Calibrator {args.fluxcal}")
+    fo.observe(args.fluxcal,obstime=args.time)
+    print(f"Slewing to Bandpass Calibrator {args.bandpass}")
+    fo.observe(args.bandpass, obstime=args.time)
+    for i in range(args.loop):
+        print(f"Slewing to Gain Calibrator {args.gaincal}")
+        fo.observe(args.gaincal,obstime=args.time)
+        fo.mixerH()
+        print(f"Slewing to Science Target {args.source}")
+        fo.observe(args.source,obstime=args.time)
+    print(f"Final Gain Calibrator {args.gaincal}")
+    fo.observe(args.gaincal,obstime=args.time)
